ea97c042a98ad19fda2d3bd973ffbe3882ae14fd
Joel Lonneker <joel.lonneker@gmail.com>
2018-02-20 11:40:04 -0800

    fixed to pass lint

--- src/sayHello.js
+++ src/sayHello.js
@@ -1,19 +1,19 @@
 exports.greet = function greet(name) {
- if (name == null) {
-  return 'Hello, friend';
- } else {
-  return 'Hello, ' + name;
- }
+	if (name == null) {
+		return 'Hello, friend';
+	} else {
+		return 'Hello, ' + name;
+	}
 };
 
 exports.salutation = function salutation(time) {
- if (time < 12) {
-  return 'Good morning!';
- } 
- if (time >= 12 && time <= 18) {
-  return 'Good afternoon!';
- }
- else {
-   return 'Good evening!'
-  }
- };
+	if (time < 12) {
+		return 'Good morning!';
+	} 
+	if (time >= 12 && time <= 18) {
+		return 'Good afternoon!';
+	}
+	else {
+		return 'Good evening!'
+	}
+};
