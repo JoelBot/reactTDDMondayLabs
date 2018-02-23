ff76b753a84f92aae0b0736ccd23e2a76a228ceb
Joel Lonneker <joel.lonneker@gmail.com>
2018-02-22 22:29:13 -0800

    lab 23

--- src/components/AnswerCheck.js
+++ src/components/AnswerCheck.js
@@ -1,8 +1,9 @@
 
 import React from 'react';
+import PropType from 'prop-types';
 
 function AnswerCheck(props){
-	return(<div>{props.checkedValue === props.correctAnswer ? "correct":"wrong"}</div>);
+	return(<div>{props.checkedValue === props.correctAnswer ? 'correct':'wrong'}</div>);
 }
 
 export default AnswerCheck;
